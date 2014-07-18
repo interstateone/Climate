d255688ebcd061933e51b69e60e4ab7a291dc20b
Brandon Evans <brandon@brandonevans.ca>
2014-07-18 17:25:01 -0600

    Initial Commit

---
+++ Climate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Climate
+//
+//  Created by Brandon Evans on 2014-07-18.
+//  Copyright (c) 2014 Brandon Evans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
